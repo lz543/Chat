a8ee069685a82924d56c8c76027c38e797382fe9
lz543 <143254371+lz543@users.noreply.github.com>
2023-09-09 17:28:33 +0800

    Update main.cpp

--- server/main.cpp
+++ server/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     TcpServer *s=new TcpServer();
-    s->start("127.0.0.1",9999);
+    s->start("192.168.0.104",8888);
     return a.exec();
 }
 
