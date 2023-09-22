937de4ddf2fc5d7dc54c935669c76374c531c772
lz543 <143254371+lz543@users.noreply.github.com>
2023-09-22 22:04:35 +0800

    Update main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -7,9 +7,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    qt4 *w=new qt4();
-    w->show();
-    qt4 *w1=new qt4();
-    w1->show();
+    qt4 w;
+    w.show();
+    qt4 w1;
+    w1.show();
     return a.exec();
 }
